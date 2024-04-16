5159f5b0c97758d09df92f3e0e1213fe083e619a
Pavel <pavel191094@icloud.com>
2024-04-16 13:23:04 +0300

    Passed Lesson 1

---
+++ condition/Lesson_1.py
@@ -0,0 +1,25 @@
+# bank = int(input('Wallet count: '))
+# price = 75000
+# if bank >= price:
+#     bank -= price
+#     print('Purchase sucsessful!' + '\nYour wallet count: ', bank)
+# print('Have a good day!')
+
+
+# number = int(input('What number did I guess?: '))
+# if number != 7:
+#     print('You lost!')
+# else:
+#     print('You won!')
+# print('Game over!')
+
+
+# distance = int(input('How much km did you go? '))
+# path = 190
+# if distance < path:
+#     print('You do not get it yet')
+#     print('You need to drive', path - distance, 'km')
+# else:
+#     print('You are at the destination point')
+
+
