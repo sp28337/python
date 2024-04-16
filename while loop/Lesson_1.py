37e944c472686d90fc6206153b0f019eee486c6a
Pavel <pavel191094@icloud.com>
2024-04-16 21:52:05 +0300

    Passed Lesson 1

---
+++ while loop/Lesson_1.py
@@ -0,0 +1,35 @@
+# number = int(input('Введите число: '))
+# summ = 0
+# while number != 0:
+#     summ += number
+#     number = int(input('Введите число: '))
+# print(summ)
+
+
+# password = int(input('Enter the password: '))
+# while password != 235:
+#     print('Wrong password!\nTry again!', end=' ')
+#     password = int(input(''))
+# else:
+#     print('Correct password! Welcome!')
+
+
+# summ = 0
+# while summ < 500000:
+#     how_much = int(input('How much money do you want to save? '))
+#     summ += how_much
+# print(summ)
+
+
+# balance = int(input('Enter your card balance: '))
+# while balance > 3000:
+#     purchase = int(input('Purchase: '))
+#     balance -= purchase
+#     if balance <= 3000:
+#         print('Warning! Your balance close to zero!\nYour balance:', balance)
+
+
+# a = 7
+# while a <= 98:
+#     print(a, end=' ')
+#     a += 7
