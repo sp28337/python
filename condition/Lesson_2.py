e908c81ba22b5c069efd75eb7394a13e02ce3634
Pavel <pavel191094@icloud.com>
2024-04-16 13:31:45 +0300

    Passed Lesson 2

---
+++ condition/Lesson_2.py
@@ -0,0 +1,19 @@
+# bank = int(input('Wallet count: '))
+# price = 75000
+# if bank >= price:
+#     bank -= price
+#     print('Purchase sucsessful!' + '\nYour wallet count: ', bank)
+# else:
+#     print('Not enouth money!')
+# print('Have a good day!')
+
+
+# num_1 = int(input('Enter first number: '))
+# num_2 = int(input('Enter second number: '))
+# summ = int(input('Summa of numbers: '))
+# if summ == num_2 + num_1:
+#     print('Right answer')
+# else:
+#     print('Wrong answer!')
+
+
