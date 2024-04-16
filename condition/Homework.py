28bb2b9ca066248f03561ce32e222e69721e1e23
Pavel <pavel191094@icloud.com>
2024-04-16 14:32:52 +0300

    Homework done

---
+++ condition/Homework.py
@@ -0,0 +1,90 @@
+# wheather = int(input('Is it raining outside? '))
+# if wheather == 1:
+#     print("It's raining outside, take an umbrella!")
+# else:
+#     print('Not raining')
+
+
+# language = int(input('Enter the number of points in English language: '))
+# mathematica = int(input('Enter the number of points in mathematica: '))
+# informatica = int(input('Enter the number of points in informatica: '))
+# passing_points = 270
+# summ_points = informatica + language + mathematica
+# if summ_points < passing_points:
+#     print('You did not pass the exam')
+# else:
+#     print('You passed the exam, congratulations!')
+
+
+# day = int(input('What day it is? '))
+# if day % 2 == 0:
+#     print('You need brush your teeth')
+
+
+# price_1 = int(input('First item price: '))
+# price_2 = int(input('Second item price: '))
+# price_3 = int(input('Third item price: '))
+# summ = price_1 + price_2 + price_3
+# if summ > 10000:
+#     summ -= summ / 100 * 10
+# print('Total summa: ', summ)
+
+
+# module = int(input('Enter a number: '))
+# if module < 0:
+#     module *= -1
+# print('module =', module)
+
+
+# your_cub = int(input('Your cub points: '))
+# owner_cub = int(input('Owner cub points: '))
+# if your_cub >= owner_cub:
+#     print(your_cub - owner_cub, '\n You pay')
+# else:
+#     print('summs: ', your_cub + owner_cub, '\nOwner pay')
+# print('GAME OVER')
+
+
+# summ_to_take = int(input('Enter the summ: '))
+# if summ_to_take != 100:
+#     print('You cannot take summ like that')
+
+
+# hours = int(input('How much hours did you work? '))
+# loan_balance = int(input('Enter loan balance: '))
+# money_for_food = int(input('How much money for food? '))
+# salary = (200 * hours) / (2 ** 3) + hours
+# if salary >= loan_balance + money_for_food:
+#     print('There are enough hours. You can rest')
+# else:
+#     print('There are not enough hours. You should work!')
+
+
+# mileage = int(input('Enter your mileage: '))
+# if 99 < mileage < 1000:
+#     day = int(input('What day is it? '))
+#     summ = (mileage % 10) + (mileage // 10) % 10 + mileage // 100
+#     if summ > day:
+#         print('Reset')
+#         mileage = 0
+#         print('Mileage =', mileage)
+#     else:
+#         print('Today was not broken')
+# else:
+#     print('Mileage must be between 99 and 1000')
+
+
+# num_1 = int(input('Enter first number: '))
+# num_2 = int(input('Enter second number: '))
+# num_3 = int(input('Enter third number: '))
+# if num_1 > num_2:
+#     if num_3 > num_1:
+#         print(num_3)
+#     else:
+#         print(num_1)
+# elif num_2 > num_3:
+#     print(num_2)
+# else:
+#     print(num_3)
+
+
