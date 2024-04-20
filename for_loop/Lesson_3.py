140dee363a03a89f4211261b9b25ddda003f4541
Pavel <pavel191094@icloud.com>
2024-04-20 09:32:26 +0300

    Renamed directories

---
+++ for_loop/Lesson_3.py
@@ -0,0 +1,45 @@
+# for i in range(1, 11):
+#     print(i ** 3)
+
+
+# a = int(input('Enter number a: '))
+# b = int(input('Enter number b: '))
+# summ = 0
+# for i in range(a, b + 1):
+#     summ += i
+# print('Summa of numbers from', a, 'to', b, '=', summ)
+
+
+# a = int(input('Enter number a: '))
+# b = int(input('Enter number b: '))
+# summ = 0
+# for i in range(a, b + 1):
+#     if i % 2 != 0:
+#         summ += i
+# print('Summa of numbers from', a, 'to', b, '=', summ)
+
+
+# wake_up = int(input('When you woked up? '))
+# count = 0
+# summ_calories = 0
+# for i in range(wake_up, 23):
+#     count += 1
+#     calories = int(input('How much calories did you eat? '))
+#     summ_calories += calories
+#     if summ_calories > 2000:
+#         print('Fall asleep. ')
+#         break
+# print('Calories was eaten:', summ_calories, '\nHours was awake:', count)
+
+
+# num = int(input('Enter number: '))
+# for i in range(2, num):
+#     if num == 2:
+#         print('Number is simple')
+#         break
+#     elif num % i == 0:
+#         print('Number is not simple')
+#         break
+#     else:
+#         print('Number is simple')
+#         break
---
+++ for_loop_part_2/Lesson_1.py
@@ -0,0 +1,31 @@
+# numbers = int(input('How much numbers? '))
+# for i in range(1, numbers // 2 + 1):
+#     i *= 2
+#     print(f'{i} ** 3 = {i ** 3}')
+
+
+# duration_experiment = int(input('Enter duration of the experiment: '))
+# cells = 1
+# for i in range(3, duration_experiment + 1, 3):
+#     cells *= 2
+#     print(f'Hours passed: {i}')
+#     print(f'Cells: {cells}')
+#     duration_experiment -= 1
+#     print(f'Hours till finish: {duration_experiment}')
+
+
+# health = 100
+# meters_togo = int(input('How much meters go? '))
+# for i in range(4, meters_togo + 1, 4):
+#     print(f'I passed {i} meters')
+#     health -= 5/2
+#     if health <= 0:
+#         print('Robot disconnected')
+#         break
+#     print(f'Health is {health}')
+
+
+# num = int(input('Enter the number: '))
+# for i in range(1, num // 2 + 1):
+#     i = i * 2 - 1
+#     print(f'{i} ** 2 = {i ** 2}')
