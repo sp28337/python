3fa093b05b608422a2b47dc128f2421899a2bad0
Pavel <pavel191094@icloud.com>
2024-04-20 10:15:18 +0300

    Passed Lesson 1

---
+++ for_with_str/Lesson_1.py
@@ -0,0 +1,23 @@
+# count = 0
+# for i in range(5):
+#     answer = input('Who wrote Evgeniy Onegin? ')
+#     if answer == 'Pushkin':
+#         print('right')
+#         break
+#     else:
+#         count += 1
+# print(f"Bad marks: {count}")
+
+
+# task = True
+# while task:
+#     question = input("Did you do yesterday's task? ")
+#     if question == 'yes':
+#         task = False
+
+
+# name = input('What is your name? ')
+# print(f"{name} buy an elefant!")
+# while True:
+#     answer = input('')
+#     print(f"Everyone tells {answer} but buy an elefant!")
