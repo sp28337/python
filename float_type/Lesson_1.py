99053b4b681e6b5567d2cb96908fc651b1b54b4b
Pavel <pavel191094@icloud.com>
2024-04-25 11:14:49 +0300

    Passed Lesson 1

---
+++ float_type/Lesson_1.py
@@ -0,0 +1,41 @@
+# bet = int(input('What is your bet? '))
+# k = float(input('Enter coefficient '))
+# win = bet * k
+# print('Potential win is {}'.format(round(win, 2)))
+
+
+# age = int(input('What is your age? '))
+# t = float(input('What is your temperature? '))
+# gift = age * 1.5 * t
+# print('Your gift is {} dollars'.format(gift))
+
+
+# weight = float(input('What is your weight? '))
+# height = float(input('What is your height? (Enter in meters) '))
+# bmi = round(weight / height ** 2, 2)
+# print('Your body mass index is: {}'.format(bmi), end=' ')
+# if bmi < 18.5:
+#     print('Shortage!')
+# elif 18.5 <= bmi < 25:
+#     print('Normal')
+# elif 25 <= bmi < 30:
+#     print('Excess weight!')
+# else:
+#     print('FAT!!!')
+
+
+# summ = 0
+# jury = int(input('How many members are on the jury? '))
+# for member in range(1, jury + 1):
+#     points = int(input('Show your points: '))
+#     if 0 <= points <= 10:
+#         print('Member of the jury under number {} gives {} point(s)'.format(member, points))
+#     else:
+#         print('Error! Points must be more than zero and less than eleven. Try again')
+#         points = int(input('Show your points: '))
+#         print('Member of the jury under number {} gives {} point(s)'.format(member, points))
+#     summ += points
+# average = round(summ / jury, 1)
+# print(f'Summa of points is {summ}, average is {average}')
+
+
