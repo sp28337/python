e4b935ac3d001283fd4c7a45ca59e701a174ea27
Pavel <pavel191094@icloud.com>
2024-04-29 15:25:11 +0300

    Lesson 3 passed

---
+++ functions/Lesson_3.py
@@ -0,0 +1,36 @@
+# def average(a, b):
+#     average_n = (a + b) / 2
+#     print('Average is', average_n)
+#
+#
+# a_num = int(input('Enter number A: '))
+# b_num = int(input('Enter number B: '))
+#
+# average(a_num, b_num)
+
+
+# def address(surname, name, country, city, street, home):
+#     print(f"Surname: {surname}\nName: {name}\nCountry: {country}\nCity: {city}\nStreet: {street}\nHome: {home}\n")
+#
+#
+# address('dTsdsasdrakanfsaov', 'Padavedla', 'Rdussdasia', 'Vyaznsaiki', 'Shsafkosaflna23wya', '3216')
+# address('Taarakaasnsdova', 'Krisastina', 'Rfsussasiag', 'Vyaasfzniki', 'Shko321lnaya', '32326')
+# address('dTaarakadnoava', 'Lucayna', 'Rfduffsfsfdia', 'Vasyaznasiki', 'Shkofsalnaya', '33126')
+
+
+# import math
+# print('What do you want?\nFind out the distance from you to point - enter 0\n'
+#       'Find out the distance between 2 points - enter 1')
+# answer = int(input())
+# if answer == 0:
+#     x = float(input('Enter X coordinates: '))
+#     y = float(input('Enter Y coordinates: '))
+#     distance = math.sqrt(x ** 2 + y ** 2)
+#     print('Distance to point is', distance)
+# elif answer == 1:
+#     x1 = float(input('Enter X1 coordinates: '))
+#     y1 = float(input('Enter Y1 coordinates: '))
+#     x2 = float(input('Enter X2 coordinates: '))
+#     y2 = float(input('Enter Y2 coordinates: '))
+#     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+#     print('Distance to point is', distance)
