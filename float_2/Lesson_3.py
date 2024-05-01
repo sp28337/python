fecb9450ca36969ee0f4bfbf1384795bccd37b64
Pavel <pavel191094@icloud.com>
2024-05-01 17:58:06 +0300

    Passed Lesson 3

---
+++ float_2/Lesson_3.py
@@ -0,0 +1,24 @@
+# tax = float(input("Enter the country's budget: "))
+# new_tax = float(input("New tax: "))
+# if tax + new_tax > tax:
+#     print('Budget will increase')
+#     print('Difference =', (tax + new_tax) - tax)
+# else:
+#     print('Difference =', (tax + new_tax) - tax)
+#     print('Budget will not increase')
+
+
+# def eqv(n1, n2, n3):
+#     if abs((n1 + n2) - n3) < 1e-15:
+#         answer = True
+#     else:
+#         answer = False
+#     return answer
+#
+#
+# num1 = float(input('Enter number 1: '))
+# num2 = float(input('Enter number 2: '))
+# num3 = float(input('Enter number 3: '))
+#
+# answ = eqv(num1, num2, num3)
+# print(answ)
