64c2f20f5d633fe8e12be4fee2862ca5ff9aa362
Pavel <pavel191094@icloud.com>
2024-05-03 19:01:54 +0300

    Passed Lesson 1

---
+++ lists/Lesson_1.py
@@ -0,0 +1,26 @@
+# numbers = [3, 7, 5]
+# while True:
+#     number = int(input('Новое число: '))
+#     numbers.append(number)
+#     print('Текущий список чисел:', numbers)
+#     for i in numbers:
+#         print(i ** 2, i ** 3, i ** 4)
+#     print()
+
+
+# numbers = []
+# for i in range(101):
+#     numbers.append(i)
+# print(numbers)
+
+
+# id_emp = []
+# employees = int(input('How many employees in the office? '))
+# for _ in range(4):
+#     id_num = int(input("Enter employee's id: "))
+#     id_emp.append(id_num)
+# seek = int(input('What id we are seeking for? '))
+# if seek in id_emp:
+#     print('Employee at work')
+# else:
+#     print('Employee is not at work!')
