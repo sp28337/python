d5e40ee508a0908aa39e5469374edfa368aacbe0
Pavel <pavel191094@icloud.com>
2024-05-11 09:51:27 +0300

    Passed Lesson_2

---
+++ list_comprehensions/Lesson_2.py
@@ -0,0 +1,18 @@
+# a_num = int(input('Enter number 1: '))
+# b_num = int(input('Enter number 2: '))
+# even_list = [x for x in range(a_num, b_num + 1) if x % 2 == 0]
+# print(even_list)
+
+
+# original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
+# correct_list = [(x if x > 0 else 0) for x in original_prices]
+# print(correct_list)
+
+
+# import  random
+#
+# squad_1 = [random.randint(50, 80) for x in range(10)]
+# squad_2 = [random.randint(30, 60) for x in range(10)]
+# squad_3 = [('Died' if squad_1[x] + squad_2[x] > 100 else 'Survived') for x in range(10)]
+#
+# print(f"Damage of first squad: {squad_1}\nDamage of second squad: {squad_2}\nStatus of third squad: {squad_3}")
