f1746a05cc661906b25b3faee170148eca1838b2
Pavel <pavel191094@icloud.com>
2024-05-13 18:31:18 +0300

    Solved task 2

--- 15_Basic_collections_strings/Lesson_1.py
+++ 15_Basic_collections_strings/Lesson_1.py
@@ -4,3 +4,8 @@
 # order_number = input('Enter your order number: ')
 # print('Hello {username}! Number of your order is {order}. Have a good day!'.format(username=name, order=order_number))
 
+# Task 2. Debts
+#
+# name = input('Enter name: ')
+# debt = int(input('Enter the debt: '))
+# print('{0}! Hello {0}! How are you, {0}? Where are my {1} dollars? {0}!'.format(name, debt))
