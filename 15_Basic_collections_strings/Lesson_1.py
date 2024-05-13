aea33314ac4c78c02268732d6a03973c8da040fd
Pavel <pavel191094@icloud.com>
2024-05-13 18:26:09 +0300

    Solved task 1

---
+++ 15_Basic_collections_strings/Lesson_1.py
@@ -0,0 +1,6 @@
+# Task 1. Order
+#
+# name = input('Enter your name: ')
+# order_number = input('Enter your order number: ')
+# print('Hello {username}! Number of your order is {order}. Have a good day!'.format(username=name, order=order_number))
+
