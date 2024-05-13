95f8a3fd9c8e8a4c5d52cc6ce466e049e14e7807
Pavel <pavel191094@icloud.com>
2024-05-13 18:47:15 +0300

    Solved task 3

--- 15_Basic_collections_strings/Lesson_1.py
+++ 15_Basic_collections_strings/Lesson_1.py
@@ -9,3 +9,18 @@
 # name = input('Enter name: ')
 # debt = int(input('Enter the debt: '))
 # print('{0}! Hello {0}! How are you, {0}? Where are my {1} dollars? {0}!'.format(name, debt))
+
+# Task 3. IP-address
+#
+# ip_address = '{0}.{1}.{2}.{3}'
+# ip = []
+# counter = 0
+# while counter < 4:
+#     num = int(input('Enter number: '))
+#     if 0 <= num < 256:
+#         ip.append(num)
+#         counter += 1
+#     else:
+#         print('Number must be between 0 and 255! Try again.')
+#
+# print(ip_address.format(ip[0], ip[1], ip[2], ip[3]))
