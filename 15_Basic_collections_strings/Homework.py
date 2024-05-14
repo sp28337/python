8db251b82c3424cf4ba7e45cb7f5fe0775d3d9c6
Pavel <pavel191094@icloud.com>
2024-05-14 15:31:40 +0300

    Solved task 1 Restaurant's menu

---
+++ 15_Basic_collections_strings/Homework.py
@@ -0,0 +1,10 @@
+# Task 1. Restaurant's menu
+#
+# while True:
+#     menu = input('Available menu: ')
+#     if ';' in menu:
+#         break
+#     else:
+#         print('Error: dishes must be separated by ;')
+#
+# print('At the moment we have:', menu.replace(';', ', '))
