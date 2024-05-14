bd87c704b5ccf422cff5d82a6848d56806b99aec
Pavel <pavel191094@icloud.com>
2024-05-14 09:37:20 +0300

    Solved task 1

---
+++ 15_Basic_collections_strings/Lesson_2.py
@@ -0,0 +1,6 @@
+# Task 1. Improved Linguistics 2
+#
+# three_words_list = input('Enter three words separated by commas: ').split(', ')
+# text = input('Enter the text: ')
+# for word in three_words_list:
+#     print('word {word} uses in text for {count} times'.format(word=word, count=text.count(word)))
