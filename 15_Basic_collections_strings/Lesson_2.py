9d3a86eff821594439d26f3989fa165813ea9230
Pavel <pavel191094@icloud.com>
2024-05-14 10:13:48 +0300

    Solved task 3 v1

--- 15_Basic_collections_strings/Lesson_2.py
+++ 15_Basic_collections_strings/Lesson_2.py
@@ -13,3 +13,23 @@
 # print('Fixed text: {text}'.format(text=str_message))
 
 
+# Task 3. Splitters of symbols
+
+while True:
+    pattern_of_congratulations = input('Enter congratulations pattern (must contain structures: {name} and {age}): ')
+    if '{name}' and '{age}' in pattern_of_congratulations:
+        break
+    print('Error: must contain structures: {name} and {age}.')
+
+list_people = input('Enter list of people separated by commas: ').split(', ')
+ages_list = input('Enter age of people separated by spaces: ').split()
+
+congratulations = [pattern_of_congratulations.format(name=list_people[i], age=ages_list[i])
+                   for i in range(len(list_people))]
+
+print('\n'.join(congratulations))
+
+print('\nBirthday people: ', end='')
+for i in range(len(list_people)):
+    print(' '.join([list_people[i], ages_list[i]]), end=', ')
+
