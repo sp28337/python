c450a3bde6cc1633ee3a0b11e04a8f2cebd6ffc8
Pavel <pavel191094@icloud.com>
2024-05-14 15:49:34 +0300

    Solved task 2 Longest word

--- 15_Basic_collections_strings/Homework.py
+++ 15_Basic_collections_strings/Homework.py
@@ -8,3 +8,16 @@
 #         print('Error: dishes must be separated by ;')
 #
 # print('At the moment we have:', menu.replace(';', ', '))
+
+
+# Task 2. Longest word
+#
+# text = input('Enter text: ')
+# list_words = text.split()
+# last_word = list_words[0]
+#
+# for word in list_words:
+#     if len(word) > len(last_word):
+#         last_word = word
+#
+# print(last_word, len(last_word))
