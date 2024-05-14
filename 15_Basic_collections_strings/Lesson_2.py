e695598aa9ecec97e491c0d4bc44a01dbdb65f02
Pavel <pavel191094@icloud.com>
2024-05-14 09:45:07 +0300

    Solved task 2

--- 15_Basic_collections_strings/Lesson_2.py
+++ 15_Basic_collections_strings/Lesson_2.py
@@ -4,3 +4,12 @@
 # text = input('Enter the text: ')
 # for word in three_words_list:
 #     print('word {word} uses in text for {count} times'.format(word=word, count=text.count(word)))
+
+
+# Task 2. Grandmother
+#
+# message = input('Enter the message: ').split()
+# str_message = ' '.join(message)
+# print('Fixed text: {text}'.format(text=str_message))
+
+
