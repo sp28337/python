d84fd67c600c1e8e54fe85f6377011992ad7850d
Pavel <pavel191094@icloud.com>
2024-05-15 18:32:15 +0300

    Solved task 2 Fruit crisis

--- 16_Dictionaries_and_Sets/Lesson_2.py
+++ 16_Dictionaries_and_Sets/Lesson_2.py
@@ -19,3 +19,26 @@
 #     print(big_storage.get(goods_name))
 # else:
 #     print('Error: there are not such position like that.')
+
+
+# Task 2. Fruit crisis
+#
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,
+#     'grapefruit': 300.40,
+#     'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+# }
+#
+# print('Total profit: {0}'.format(sum(incomes.values())))
+# min_value = list(incomes.values())
+# min_key = list(incomes.keys())
+# deleted_fruit = min_key[min_value.index(min(min_value))]
+# print('Smallest profit from {0}. It contains {1} dollars'.format(deleted_fruit, min(min_value)))
+# incomes.pop(deleted_fruit)
+# print('Final dictionary:', incomes)
