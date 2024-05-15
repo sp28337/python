26f1e4da4aa6f84523045c38f0e544ac38d94eb2
Pavel <pavel191094@icloud.com>
2024-05-15 15:19:25 +0300

    Solved task 2 Student

--- 16_Dictionaries_and_Sets/Lesson_1.py
+++ 16_Dictionaries_and_Sets/Lesson_1.py
@@ -6,3 +6,18 @@
 #     new_dict[key] = key ** 2
 #
 # print(new_dict)
+
+
+# Task 2. Student
+#
+# student_info = input('Enter info of student separated by spaces(name, surname, city, study place, grates): ').split()
+# student = dict()
+# student['Name'] = student_info[0]
+# student['Surname'] = student_info[1]
+# student['City'] = student_info[2]
+# student['Study place'] = student_info[3]
+# student['Grates'] = []
+# for i_grates in student_info[-4:]:
+#     student['Grates'].append(i_grates)
+# for i in student:
+#     print(i, '-', student[i])
