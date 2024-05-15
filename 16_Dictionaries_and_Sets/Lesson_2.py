cdb915cb296cf72483a0bc7f5ec642e2c531cfb7
Pavel <pavel191094@icloud.com>
2024-05-15 16:11:13 +0300

    Solved task 1 Stocks

---
+++ 16_Dictionaries_and_Sets/Lesson_2.py
@@ -0,0 +1,21 @@
+# Task 1. Stocks
+#
+# small_storage = {
+#     'гвозди': 5000,
+#     'шурупы': 3040,
+#     'саморезы': 2000
+# }
+#
+# big_storage = {
+#     'доски': 1000,
+#     'балки': 150,
+#     'рейки': 600
+# }
+#
+# big_storage.update(small_storage)
+#
+# goods_name = input("Enter good's name: ")
+# if big_storage.get(goods_name):
+#     print(big_storage.get(goods_name))
+# else:
+#     print('Error: there are not such position like that.')
