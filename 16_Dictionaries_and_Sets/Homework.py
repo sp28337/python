d3637f913c69300d222bae5dc17cb3314fa55a0b
Pavel <pavel191094@icloud.com>
2024-05-17 20:27:46 +0300

    Solved task 2 Geography

--- 16_Dictionaries_and_Sets/Homework.py
+++ 16_Dictionaries_and_Sets/Homework.py
@@ -20,3 +20,31 @@
 #
 # print(f"\nTotal time sounding: {sum(total_sound)}")
 
+
+# Task 2. Geography
+#
+# def find_country(num_countr):
+#     dict_country = dict()
+#     list_country = input('').split()
+#     country = list_country[0]
+#     list_country.remove(list_country[0])
+#     for city in list_country:
+#         dict_country[city] = country
+#     return dict_country
+#
+#
+# number_countries = int(input('Enter number of countries: '))
+#
+# total_dict = dict()
+# for i in range(1, number_countries + 1):
+#     print(f"{i} country:", end='')
+#     total_dict.update(find_country(number_countries))
+#
+# for j in range(1, 4):
+#     print(f"\n{j} city:", end='')
+#     city_name = input('')
+#     if total_dict.get(city_name):
+#         print(f"{city_name} city is a part of {total_dict.get(city_name)}")
+#     else:
+#         print(total_dict.get(city_name, f"No info about {city_name}"))
+
