89daa6e796dc02950dad2b692c6efa21d58bff74
Pavel <pavel191094@icloud.com>
2024-05-17 16:30:59 +0300

    Solved task 1 Songs 2

---
+++ 16_Dictionaries_and_Sets/Homework.py
@@ -0,0 +1,22 @@
+# Task 1. Songs 2
+#
+# violator_songs = {
+#     'World in My Eyes': 4.86,
+#     'Sweetest Perfection': 4.43,
+#     'Personal Jesus': 4.56,
+#     'Halo': 4.9,
+#     'Waiting for the Night': 6.07,
+#     'Enjoy the Silence': 4.20,
+#     'Policy of Truth': 4.76,
+#     'Blue Dress': 4.29,
+#     'Clean': 5.83
+# }
+#
+# total_sound = []
+# take_songs = int(input('How many songs to choose? '))
+# for _ in range(take_songs):
+#     name = input('Enter name of song: ')
+#     total_sound.append(violator_songs.get(name, 0))
+#
+# print(f"\nTotal time sounding: {sum(total_sound)}")
+
