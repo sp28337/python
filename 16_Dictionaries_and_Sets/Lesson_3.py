f167f7c3cc30c59e4eecdf84b58bbd69b2f34715
Pavel <pavel191094@icloud.com>
2024-05-17 14:32:07 +0300

    Solved task 2 Players

--- 16_Dictionaries_and_Sets/Lesson_3.py
+++ 16_Dictionaries_and_Sets/Lesson_3.py
@@ -40,3 +40,32 @@
 # name_to_find = input('What name need to find? ')
 # question = find_name(family_member, name_to_find)
 # print(question)
+
+
+# Task 2. Players
+#
+# players_dict = {
+#     1: {'name': 'Vanya', 'team': 'A', 'status': 'Rest'},
+#     2: {'name': 'Lena', 'team': 'B', 'status': 'Training'},
+#     3: {'name': 'Maxim', 'team': 'C', 'status': 'Travel'},
+#     4: {'name': 'Egor', 'team': 'C', 'status': 'Rest'},
+#     5: {'name': 'Andrei', 'team': 'A', 'status': 'Training'},
+#     6: {'name': 'Sasha', 'team': 'A', 'status': 'Rest'},
+#     7: {'name': 'Alina', 'team': 'B', 'status': 'Rest'},
+#     8: {'name': 'Masha', 'team': 'C', 'status': 'Travel'}
+# }
+#
+# resting_A_players = list()
+# workout_B_players = list()
+# traveling_C_players = list()
+#
+# for player in players_dict.values():
+#     if player.get('team') == 'A' and player.get('status') == 'Rest':
+#         resting_A_players.append(player.get('name'))
+#     elif player.get('team') == 'B' and player.get('status') == 'Training':
+#         workout_B_players.append(player.get('name'))
+#     elif player.get('team') == 'C' and player.get('status') == 'Travel':
+#         traveling_C_players.append(player.get('name'))
+#
+# print("Team A resting members: {0}\nTeam B training members: {1}\nTeam C traveling members: {2}"
+#       .format(', '.join(resting_A_players), ', '.join(workout_B_players), ', '.join(traveling_C_players)))
