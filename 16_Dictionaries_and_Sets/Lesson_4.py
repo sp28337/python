ddd34cafcd755798642f55e411134be458bea63f
Pavel <pavel191094@icloud.com>
2024-05-17 15:08:50 +0300

    Solved task 1 Punctuation

---
+++ 16_Dictionaries_and_Sets/Lesson_4.py
@@ -0,0 +1,11 @@
+# Task 1. Punctuation
+#
+# symbols = set()
+# text = input('Enter the string: ')
+# count = 0
+# for i in text:
+#     if i == '.' or i == ',' or i == ';' or i == '!' or i == '?':
+#         symbols.add(i)
+#         count += 1
+# print(symbols)
+# print(f"Count of symbols: {count}")
