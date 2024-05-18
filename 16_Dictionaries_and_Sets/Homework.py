e21872e17a2bc6dae602e03cc80cdefbd1528ae1
Pavel <pavel191094@icloud.com>
2024-05-18 15:46:57 +0300

    Solved task 3 Krypto

--- 16_Dictionaries_and_Sets/Homework.py
+++ 16_Dictionaries_and_Sets/Homework.py
@@ -48,3 +48,64 @@
 #     else:
 #         print(total_dict.get(city_name, f"No info about {city_name}"))
 
+
+# Task 3. Krypto
+#
+# data = {
+#     "address": "0x544444444444",
+#     "ETH": {
+#         "balance": 444,
+#         "totalIn": 444,
+#         "totalOut": 4
+#     },
+#     "count_txs": 2,
+#     "tokens": [
+#         {
+#             "fst_token_info": {
+#                 "address": "0x44444",
+#                 "name": "fdf",
+#                 "decimals": 0,
+#                 "symbol": "dsfdsf",
+#                 "total_supply": "3228562189",
+#                 "owner": "0x44444",
+#                 "last_updated": 1519022607901,
+#                 "issuances_count": 0,
+#                 "holders_count": 137528,
+#                 "price": False
+#             },
+#             "balance": 5000,
+#             "totalIn": 0,
+#             "totalOut": 0
+#         },
+#         {
+#             "sec_token_info": {
+#                 "address": "0x44444",
+#                 "name": "ggg",
+#                 "decimals": "2",
+#                 "symbol": "fff",
+#                 "total_supply": "250000000000",
+#                 "owner": "0x44444",
+#                 "last_updated": 1520452201,
+#                 "issuances_count": 0,
+#                 "holders_count": 20707,
+#                 "price": False
+#             },
+#             "balance": 500,
+#             "totalIn": 0,
+#             "total_out": 0
+#         }
+#     ]
+# }
+#
+#
+# for key in data:
+#     print(key, ':', data[key])
+#
+# data['ETH'].setdefault('total_diff', 100)
+# data['tokens'][0]['fst_token_info']['name'] = 'doge'
+# data['ETH']['totalOut'] = data['tokens'][0]['totalOut']
+# del data['tokens'][0]['totalOut']
+# data["tokens"][1]["sec_token_info"].keys()
+# value = data["tokens"][1]["sec_token_info"].pop('price')
+# data["tokens"][1]["sec_token_info"]['total_price'] = value
+# print(data["tokens"][1]["sec_token_info"])
