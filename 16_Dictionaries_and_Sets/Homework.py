a38a80afdfc23beaf94acd2ad6cfd3686356ca6a
Pavel <pavel191094@icloud.com>
2024-05-18 16:32:53 +0300

    Solved task 4 Goods

--- 16_Dictionaries_and_Sets/Homework.py
+++ 16_Dictionaries_and_Sets/Homework.py
@@ -109,3 +109,48 @@
 # value = data["tokens"][1]["sec_token_info"].pop('price')
 # data["tokens"][1]["sec_token_info"]['total_price'] = value
 # print(data["tokens"][1]["sec_token_info"])
+
+
+# Task 4. Goods
+#
+# def total_summ_and_profit(list_items):
+#     total_sum = total_profit = 0
+#     for i in list_items:
+#         total_sum += i['quantity']
+#         total_profit += i['price'] * i['quantity']
+#     print(total_sum, 'шт, стоимость', total_profit, 'руб')
+#
+# goods = {
+#     'Лампа': '12345',
+#     'Стол': '23456',
+#     'Диван': '34567',
+#     'Стул': '45678',
+# }
+#
+# store = {
+#     '12345': [
+#         {'quantity': 27, 'price': 42},
+#     ],
+#     '23456': [
+#         {'quantity': 22, 'price': 510},
+#         {'quantity': 32, 'price': 520},
+#     ],
+#     '34567': [
+#         {'quantity': 2, 'price': 1200},
+#         {'quantity': 1, 'price': 1150},
+#     ],
+#     '45678': [
+#         {'quantity': 50, 'price': 100},
+#         {'quantity': 12, 'price': 95},
+#         {'quantity': 43, 'price': 97},
+#     ],
+# }
+#
+#
+# for item in goods.keys():
+#     print(item, '-', end=' ')
+#     for i_store in store.keys():
+#         if goods[item] == i_store:
+#             total_summ_and_profit(store[i_store])
+
+
