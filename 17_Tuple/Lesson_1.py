fee5c5455fbbe97a64d3b8d21dca4f3f3d740287
Pavel <pavel191094@icloud.com>
2024-05-19 21:04:20 +0300

    Solved task 1 Making tuples

---
+++ 17_Tuple/Lesson_1.py
@@ -0,0 +1,12 @@
+# Task 1. Making tuples
+#
+# import random
+#
+# tuple1 = tuple(random.randint(0, 5) for _ in range(10))
+# tuple2 = tuple(random.randint(-5, 0) for _ in range(10))
+# tuple3 = tuple1 + tuple2
+# print(tuple1)
+# print(tuple2)
+# print(tuple3)
+# print(tuple3.count(0))
+
