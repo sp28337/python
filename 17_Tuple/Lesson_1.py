3aa652254310f1de1e75c76c66168fea6e30eb10
Pavel <pavel191094@icloud.com>
2024-05-19 21:17:06 +0300

    Solved task 2 Cylinder

--- 17_Tuple/Lesson_1.py
+++ 17_Tuple/Lesson_1.py
@@ -10,3 +10,23 @@
 # print(tuple3)
 # print(tuple3.count(0))
 
+
+# Task 2. Cylinder
+#
+# import math
+#
+#
+# def find_side(r, h):
+#     new_side = 2 * math.pi * r * h
+#     s = math.pi * r ** 2
+#     full = new_side + 2 * s
+#     return new_side, full
+#
+#
+# radius = float(input('Enter radius of cylinder: '))
+# height = float(input('Enter height of cylinder: '))
+#
+# result = find_side(radius, height)
+# for value in result:
+#     print(round(value, 2))
+#
