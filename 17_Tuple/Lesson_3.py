304a6f738b192f2a9a6f6232cab1104623f5580c
Pavel <pavel191094@icloud.com>
2024-05-21 16:00:29 +0300

    Solved task 2 Server

--- 17_Tuple/Lesson_3.py
+++ 17_Tuple/Lesson_3.py
@@ -13,3 +13,29 @@
 #
 # for fruit_name, estimate in incomes.items():
 #     print('{fruit} -- {price}'.format(fruit=fruit_name, price=estimate))
+
+
+# Task 2. Server
+#
+# server_data = {
+#     "server": {
+#         "host": "127.0.0.1",
+#         "port": "10"
+#     },
+#     "configuration": {
+#         "access": "true",
+#         "login": "Ivan",
+#         "password": "qwerty"
+#     }
+# }
+#
+# for i_key, i_value in server_data.items():
+#     print('{key}:'.format(key=i_key))
+#     for j_key, j_value in i_value.items():
+#         print('    {key}: {value}'.format(key=j_key, value=j_value))
+#     print()
+
+
+# Task 3. In a string
+#
+# print([i_value for i_key, i_value in {0: 0, 1: 100, 2: 144, 3: 20, 4: 19}.items() if i_key % 2 == 0])
