43fee8c8838d3d26dcdb4c42a919d4fa5b4dfde4
Pavel <pavel191094@icloud.com>
2024-05-21 13:15:27 +0300

    Solved task 2 Dicts from lists

--- 17_Tuple/Lesson_2.py
+++ 17_Tuple/Lesson_2.py
@@ -5,3 +5,30 @@
 #     if sym == '~':
 #         print(index, end=' ')
 
+
+# Task 2. Dicts from lists
+#
+#
+# import random, string
+#
+#
+# def make_dict_from_list(some_list):
+#     data_dict = dict()
+#     for index, symbol in enumerate(some_list):
+#         data_dict[index] = symbol
+#     return data_dict
+#
+#
+# alphabet = string.ascii_lowercase
+# print(alphabet)
+#
+# list_1 = [random.choice(alphabet) for _ in range(10)]
+# list_2 = [random.choice(alphabet) for _ in range(10)]
+#
+# print(f"First list: {list_1}\nSecond list {list_2}")
+#
+# dict1 = make_dict_from_list(list_1)
+# dict2 = make_dict_from_list(list_2)
+#
+# print(f"\nFirst dict: {dict1}\nSecond dict: {dict2}")
+
