99357e534041b118298b0d8fa8c8fd76cba7918c
Pavel <pavel191094@icloud.com>
2024-05-21 15:47:37 +0300

    Solved task 1 Crisis is over

---
+++ 17_Tuple/Lesson_3.py
@@ -0,0 +1,15 @@
+# Task 1. Crisis is over
+#
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,
+#     'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+# }
+#
+# for fruit_name, estimate in incomes.items():
+#     print('{fruit} -- {price}'.format(fruit=fruit_name, price=estimate))
