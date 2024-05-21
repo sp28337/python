5ea3ed25ada8471d9855155c88e1c3b55c1cd161
Pavel <pavel191094@icloud.com>
2024-05-21 19:14:00 +0300

    Solved task 1 Passport data

---
+++ 17_Tuple/Lesson_4.py
@@ -0,0 +1,24 @@
+# Task 1. Passport data
+#
+# def find_name_surname(ser, num, dictionary):
+#     for key, value in dictionary.items():
+#         if ser and num in key:
+#             name_f = value[0]
+#             surname_f = value[1]
+#     return name_f, surname_f
+#
+#
+# data = {
+#     (5000, 123456): ('Иванов', 'Василий'),
+#     (6000, 111111): ('Иванов', 'Петр'),
+#     (7000, 222222): ('Медведев', 'Алексей'),
+#     (8000, 333333): ('Алексеев', 'Георгий'),
+#     (9000, 444444): ('Георгиева', 'Мария')
+# }
+#
+# series = int(input('Enter series of the passport: '))
+# number = int(input('Enter number of the passport: '))
+#
+# surname, name = find_name_surname(series, number, data)
+# print(name, surname)
+
