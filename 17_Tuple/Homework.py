8d644ec9185b86cd4c23c883d14e5c484d48e08e
Pavel <pavel191094@icloud.com>
2024-05-22 14:29:29 +0300

    Solved task 4. Players

--- 17_Tuple/Homework.py
+++ 17_Tuple/Homework.py
@@ -105,3 +105,15 @@
 #
 # print(another_tuple)
 
+
+# Task 4. Players
+#
+# players = {
+#     ("Ivan", "Volkin"): (10, 5, 13),
+#     ("Bob", "Robbin"): (7, 5, 14),
+#     ("Rob", "Bobbin"): (12, 8, 2)
+# }
+#
+# new_data = [key + val for key, val in players.items()]
+# print(new_data)
+
