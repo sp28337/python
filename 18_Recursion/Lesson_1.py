371bbd00622731f82b2d0820da567a047a55a25e
Pavel <pavel191094@icloud.com>
2024-05-27 08:59:33 +0300

    Solved task 1 Challenge

---
+++ 18_Recursion/Lesson_1.py
@@ -0,0 +1,11 @@
+# Task 1. Challenge
+#
+# def find_factorial(num):
+#     if num == 1:
+#         return 1
+#     fact_n_minus_1 = find_factorial(num - 1)
+#     return fact_n_minus_1 * num
+#
+#
+# num_factorial = find_factorial(5)
+# print(num_factorial)
