123220505d13ab9eae1e09f4072cc110b787bdc7
Pavel <pavel191094@icloud.com>
2024-05-27 09:21:12 +0300

    Solved task 2 Power of number

--- 18_Recursion/Lesson_1.py
+++ 18_Recursion/Lesson_1.py
@@ -9,3 +9,18 @@
 #
 # num_factorial = find_factorial(5)
 # print(num_factorial)
+
+
+# Task 2. Power of number
+#
+# def power(a, n):
+#     if n == 0:
+#         return 1
+#     return a * power(a, n - 1)
+#
+#
+# float_num = float(input('Введите вещественное число: '))
+# int_num = int(input('Введите степень числа: '))
+# print(float_num, '**', int_num, '=', power(float_num, int_num))
+
+
