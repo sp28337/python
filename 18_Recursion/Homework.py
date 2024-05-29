cce561ab89dd4dfb764c1ac7e1127c5a34e1ec45
Pavel <pavel191094@icloud.com>
2024-05-29 10:34:02 +0300

    Solved task 1 Challenge 2

---
+++ 18_Recursion/Homework.py
@@ -0,0 +1,12 @@
+# Task 1. Challenge 2
+#
+# def seq_num(num):
+#     if num == 0:
+#         return 1
+#     seq_num(num - 1)
+#     print(num)
+#
+#
+# number = int(input('Enter the number: '))
+# seq_num(number)
+
