baf782b7b57d0957fd44efc0c01c33d15d1cfa72
Pavel <pavel191094@icloud.com>
2024-05-29 15:35:36 +0300

    Solved task 2 My own zip 2

--- 18_Recursion/Homework.py
+++ 18_Recursion/Homework.py
@@ -10,3 +10,23 @@
 # number = int(input('Enter the number: '))
 # seq_num(number)
 
+
+# Task 2. My own zip 2
+#
+# def clone_zip(*iterables):
+#     length = min(len(item) for item in iterables)
+#     tpl = (tuple(elem[index] for elem in map(tuple, iterables))
+#            for index in range(length))
+#     return tpl
+#
+#
+# a = (10, 20, 30, 40, 50)
+# b = {1: 11, 2: 22, 3: 33, 4: 44, 5: 55}
+# c = 'abc'
+# e = {111, 222, 333, 444}
+#
+# print(*zip(a, b, c, e))
+#
+# result = clone_zip(a, b, c, e)
+#
+# print(*result)
