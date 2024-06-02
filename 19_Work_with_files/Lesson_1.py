e8f57d573c13b8b8f776c61d36d78dc1c96b3c2e
Pavel <pavel191094@icloud.com>
2024-06-02 14:30:36 +0300

    Solved task 1 Sysadmin

---
+++ 19_Work_with_files/Lesson_1.py
@@ -0,0 +1,12 @@
+# Task 1. Sysadmin
+#
+# import os
+#
+# dir_name = 'access'
+# file_name = 'admin.bat'
+#
+# absolute_path = os.path.abspath(os.path.join('..', '..', dir_name, file_name))
+# relative_path = os.path.join(dir_name, file_name)
+#
+# print('Absolute path: {0}\nRelative path: {1}'.format(absolute_path, relative_path))
+#
