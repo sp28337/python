a286d5b2f9dc98cc0878792d16ae8683a9767046
Pavel <pavel191094@icloud.com>
2024-06-06 18:10:39 +0300

    task 1 Five element

---
+++ 20_Exceptions_work_with_errors/Lesson_1.py
@@ -0,0 +1,13 @@
+# Task 1. Five element
+#
+# BRUCE_WILLIS = 42
+#
+# try:
+#     input_data = input('Введите строку: ')
+#     leeloo = int(input_data[4])
+#     result = BRUCE_WILLIS * leeloo
+#     print(f'- Leeloo Dallas! Multi-pass № {result}!')
+# except ValueError:
+#     print('Impossible convert to a number!')
+# except IndexError:
+#     print('Index out of range')
