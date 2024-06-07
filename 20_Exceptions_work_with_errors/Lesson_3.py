6ef9797eed05e0494eb52c3fdab1fa10654dfea7
Pavel <pavel191094@icloud.com>
2024-06-07 18:51:57 +0300

    task 2 Loging

--- 20_Exceptions_work_with_errors/Lesson_3.py
+++ 20_Exceptions_work_with_errors/Lesson_3.py
@@ -17,3 +17,47 @@
 #     raise
 # else:
 #     print('Program completed without any errors.')
+
+
+# Task 2. Logging
+#
+#
+# def isPalindrome(string):
+#     if len(string) < 2:
+#         return True
+#     elif string[0] == string[-1]:
+#         return isPalindrome(string[1:-1])
+#     else:
+#         return False
+#
+#
+# def add_to_errors_log(path, error):
+#     path_file = open(path, 'a', encoding='utf-8')
+#     path_file.write(error + '\n')
+#     path_file.close()
+#
+#
+# def format_row(line):
+#     for char in line:
+#         if char.isdigit():
+#             raise ValueError('There is a digit in the line!')
+#
+#     if line.endswith('\n'):
+#         line = line[:-1]
+#     return line.replace(' ', '')
+#
+#
+# words_count = 0
+# words_file = open('words.txt', encoding='utf-8')
+# for index, row in enumerate(words_file):
+#     try:
+#         row = format_row(row)
+#         if isPalindrome(row.lower()):
+#             words_count += 1
+#     except ValueError:
+#         error_name = f'ValueError: there is a digit in the line ({index + 1})!'
+#         print(error_name)
+#         add_to_errors_log('errors.log', error_name)
+# words_file.close()
+#
+# print('Count palindromes:', words_count)
