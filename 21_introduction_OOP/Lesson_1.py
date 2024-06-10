5821462b365a44168dcf0f525eda1d3b63ddffd4
Pavel <pavel191094@icloud.com>
2024-06-10 14:17:48 +0300

    task 1 Vehicle

---
+++ 21_introduction_OOP/Lesson_1.py
@@ -0,0 +1,23 @@
+# Task 1. Vehicle
+#
+# from random import randint
+#
+#
+# class Toyota:
+#     color = 'red'
+#     price = 1_000_000
+#     max_speed = 200
+#     current_speed = 0
+#
+#
+# car1 = Toyota()
+# car1.current_speed = randint(0, 200)
+#
+# car2 = Toyota()
+# car2.current_speed = randint(0, 200)
+#
+# car3 = Toyota()
+# car3.current_speed = randint(0, 200)
+#
+# print(car1.current_speed, car2.current_speed, car3.current_speed)
+# print(Toyota.current_speed)
