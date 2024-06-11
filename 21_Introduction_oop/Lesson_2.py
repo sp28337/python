9f1f85b90660e25bc9d8a2d956ed95ed74e776a3
Pavel <pavel191094@icloud.com>
2024-06-11 13:42:23 +0300

    task 2 Family

--- 21_Introduction_oop/Lesson_2.py
+++ 21_Introduction_oop/Lesson_2.py
@@ -23,3 +23,41 @@
 # car1.info()
 # car1.set_current_speed(120)
 # car1.info()
+
+
+# Task 2. Family
+#
+# class Family:
+#     surname = 'Common family'
+#     money = 100_000
+#     having_a_house = False
+#
+#     def info(self):
+#         print('Family info:\n'
+#               'Surname --> {}\n'
+#               'Money --> {}\n'
+#               'House {}\n'.format(self.surname, self.money, self.having_a_house)
+#               )
+#
+#     def earn_money(self, amount):
+#         self.money += amount
+#         print('[+] {}$\n'
+#               'Current balance: {}\n'.format(amount, self.money)
+#               )
+#
+#     def buy_house(self, price, discount=0):
+#         price -= price * discount / 100
+#         print('Try to by a house')
+#         if self.money >= price:
+#             self.money -= price
+#             self.having_a_house = True
+#             print('House successfully purchased!\n'
+#                   'Current balance: {}\n'.format(self.money))
+#         else:
+#             print('Purchased unsuccessfully, you have not enough money!\n')
+#
+#
+# my_family = Family()
+# my_family.info()
+# my_family.buy_house(1_000_000, 90)
+# my_family.info()
