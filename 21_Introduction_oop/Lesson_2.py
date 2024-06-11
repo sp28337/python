9a542e7bb625700d670fc20c753bd1f2928f2430
Pavel <pavel191094@icloud.com>
2024-06-11 13:42:01 +0300

    task 1 Vehicle 2

---
+++ 21_Introduction_oop/Lesson_2.py
@@ -0,0 +1,25 @@
+# Task 1. Vehicle 2
+#
+# class Toyota:
+#     color = 'red'
+#     price = 1_000_000
+#     max_speed = 200
+#     current_speed = 0
+#
+#     def info(self):
+#         print('\nCurrent info:\n'
+#               'Color --- {}\n'
+#               'Price --- {}\n'
+#               'Max speed --- {}\n'
+#               'Current speed --- {}'.format(self.color, self.price, self.max_speed, self.current_speed)
+#               )
+#
+#     def set_current_speed(self, speed):
+#         self.current_speed = speed
+#         print('\nCurrent speed is {}'.format(self.current_speed))
+#
+#
+# car1 = Toyota()
+# car1.info()
+# car1.set_current_speed(120)
+# car1.info()
