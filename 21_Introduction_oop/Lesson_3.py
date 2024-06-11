545e67e613a84d9bed90463c3cee221ca4722b16
Pavel <pavel191094@icloud.com>
2024-06-11 16:41:55 +0300

    task 1 Vehicle 3

---
+++ 21_Introduction_oop/Lesson_3.py
@@ -0,0 +1,28 @@
+# Task 1. Vehicle 3
+#
+# class Toyota:
+#     def __init__(self, color, price, max_speed, current_speed):
+#         self.color = color
+#         self.price = price
+#         self.max_speed = max_speed
+#         self.current_speed = current_speed
+#
+#     def info(self):
+#         print('Information\n'
+#               'Color: {}\n'
+#               'Price: {}\n'
+#               'Max speed: {}\n'
+#               'Current speed: {}\n'.format(self.color, self.price, self.max_speed, self.current_speed)
+#               )
+#
+#     def set_current_speed(self, speed):
+#         if speed > 0:
+#             self.current_speed = speed
+#         else:
+#             print('Speed cannot be less than zero!')
+#
+#
+# car1 = Toyota('Red', 1_000_000, 200, 0)
+# car1.info()
+# car1.set_current_speed(140)
+# car1.info()
