9119a6d38e9923e87bf6db788a34173761eae225
Pavel <pavel191094@icloud.com>
2024-06-12 17:53:27 +0300

    task 3 Happy farm

--- 21_Introduction_oop/Lesson_3.py
+++ 21_Introduction_oop/Lesson_3.py
@@ -63,3 +63,50 @@
 #     point2.info()
 # except ValueError as e:
 #     print(e)
+
+
+# Task 3. Happy farm
+#
+# class Potato:
+#     states = {0: 'seedling', 1: 'crossbreed', 2: 'flowering', 3: 'ripe'}
+#
+#     def __init__(self, index):
+#         self.index = index
+#         self.state = 0
+#
+#     def grow(self):
+#         if self.state < 3:
+#             self.state += 1
+#         self.print_state()
+#
+#     def is_ripe(self):
+#         if self.state == 3:
+#             return True
+#         return False
+#
+#     def print_state(self):
+#         print('Potato {} now {}'.format(self.index, Potato.states[self.state]))
+#
+#
+# class PotatoGarden:
+#
+#     def __init__(self, count):
+#         self.potatoes = [Potato(index) for index in range(1, count + 1)]
+#
+#     def grow_all(self):
+#         print('Potato growing up!')
+#         for i_potato in self.potatoes:
+#             i_potato.grow()
+#
+#     def are_all_ripe(self):
+#         if not all([i_potato.is_ripe() for i_potato in self.potatoes]):
+#             print('Potato does not ripe yet!\n')
+#         else:
+#             print('All potatoes is ripe!')
+#
+#
+# my_garden = PotatoGarden(5)
+# my_garden.are_all_ripe()
+# for _ in range(3):
+#     my_garden.grow_all()
+#     my_garden.are_all_ripe()
