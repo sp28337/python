800a3c1be27925198686c3d38f4016e45593331c
Pavel <pavel191094@icloud.com>
2024-06-12 16:02:17 +0300

    task 2 Point coordinates

--- 21_Introduction_oop/Lesson_3.py
+++ 21_Introduction_oop/Lesson_3.py
@@ -26,3 +26,40 @@
 # car1.info()
 # car1.set_current_speed(140)
 # car1.info()
+
+
+# Task 2. Point coordinates
+#
+# class Point:
+#     total_points = 0
+#     coordinates_set = set()
+#
+#     def __init__(self, x=0, y=0):
+#         if self.coordinates_exists(x, y):
+#             raise ValueError(f'Point with coordinates ({x}, {y}) already exists!')
+#         Point.coordinates_set.add((x, y))
+#         Point.total_points += 1
+#         self.x, self.y, self.number = x, y, Point.total_points
+#
+#     def coordinates_exists(self, x, y):
+#         return (x, y) in self.coordinates_set
+#
+#     def info(self):
+#         print('Information about point number {n}\n'
+#               'X = {x}\n'
+#               'Y = {y}\n'
+#               'Total points = {t}\n'.format(n=self.number,
+#                                             x=self.x,
+#                                             y=self.y,
+#                                             t=self.total_points
+#                                             )
+#               )
+#
+#
+# try:
+#     point1 = Point()
+#     point1.info()
+#     point2 = Point()
+#     point2.info()
+# except ValueError as e:
+#     print(e)
