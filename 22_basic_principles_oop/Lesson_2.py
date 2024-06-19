65a8908307f0f2553f5d4bd9f48cb9d85146f090
Pavel <pavel191094@icloud.com>
2024-06-19 09:14:51 +0300

    task 2 Robots

--- 22_basic_principles_oop/Lesson_2.py
+++ 22_basic_principles_oop/Lesson_2.py
@@ -49,3 +49,66 @@
 # ship2.attack()
 # ship1.loading()
 # ship1.sail()
+
+
+# Task 2. Robots
+#
+# class Robots:
+#     def __init__(self, model):
+#         self.__model = model
+#
+#     def operate(self):
+#         print('Robot', end=' ')
+#
+#
+# class RobotCleaner(Robots):
+#
+#     def __init__(self, model):
+#         super().__init__(self)
+#         self.__model = model
+#         self.__fullness = 0
+#         print('\n(robot model <{}> initialized)'.format(self.__model))
+#
+#     def __str__(self):
+#         return self.__model + ' cleaner robot'
+#
+#     def operate(self):
+#         super().operate()
+#         self.__fullness += 1
+#         print('{} is cleaning floor. Fullness: {}'.format(self.__model, self.__fullness))
+#
+#
+# class WarRobot(Robots):
+#     def __init__(self, model, weapon):
+#         super().__init__(self)
+#         self.__model = model
+#         self.__weapon = weapon
+#         print('\n(robot model <{}> initialized)'.format(self.__model))
+#
+#     def __str__(self):
+#         return self.__model + ' military robot'
+#
+#     def operate(self):
+#         super().operate()
+#         print('{} defended the military object'.format(self.__model))
+#
+#
+# class WarShip(Robots):
+#     def __init__(self, model, weapon):
+#         super().__init__(WarRobot)
+#         self.__model = model
+#         self.__weapon = weapon
+#         self.__deep = 0
+#
+#
+# robot_1 = RobotCleaner('R2D2')
+# print(robot_1)
+# robot_1.operate()
+#
+# robot_2 = WarRobot('C3PO', 'ak-47')
+# print(robot_2)
+# robot_2.operate()
+#
+# robot_3 = WarRobot('UnderWaterShip333', 'Torpedoes')
+# print(robot_3)
+# robot_3.operate()
