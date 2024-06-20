932c119c39851c0f8a6fba94cb506f319359c518
Pavel <pavel191094@icloud.com>
2024-06-20 14:31:28 +0300

    task 2 Karma

--- 22_basic_principles_oop/Homework.py
+++ 22_basic_principles_oop/Homework.py
@@ -130,3 +130,134 @@
 #
 #
 # main()
+
+
+# Task 2. Karma
+#
+# from random import randint
+#
+#
+# class KillError(Exception):
+#
+#     def __str__(self):
+#         return 'KillError'
+#
+#
+# class DrunkError(Exception):
+#     def __str__(self):
+#         return 'DrunkError'
+#
+#
+# class CarCrashError(Exception):
+#     def __str__(self):
+#         return 'CarCrashError'
+#
+#
+# class GluttonyError(Exception):
+#     def __str__(self):
+#         return 'GluttonyError'
+#
+#
+# class DepressionError(Exception):
+#     def __str__(self):
+#         return 'DepressionError'
+#
+#
+# class Life:
+#     """
+#     class Life for simulating life
+#
+#     __carma: points of karma
+#
+#     Args:
+#         days (int): count of days
+#     Attributes:
+#           my_exceptions (tuple): tuple of custom exceptions
+#     """
+#
+#     __karma = 0
+#     my_exceptions = (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError)
+#
+#     def __init__(self, days=0):
+#         print('(New life was initialized)')
+#         self.__days = 0
+#
+#     def set_karma(self, val):
+#         """
+#         Setter for karma
+#
+#         :param val: value to increase karma
+#         :type val: int
+#         """
+#         self.__karma += val
+#         print('Carma has increased on {} points'.format(val))
+#
+#     def set_days(self):
+#         """
+#         Setter for days. Increase days count +1 each time it is invoked.
+#         """
+#         self.__days += 1
+#         print('Day number {}'.format(self.__days))
+#
+#     def get_karma(self):
+#         """
+#         Getter for karma
+#
+#         :return: __karma
+#         :rtype: int
+#         """
+#         return self.__karma
+#
+#     def get_days(self):
+#         """
+#         Getter for days
+#
+#         :return: __days
+#         :rtype: int
+#         """
+#         return self.__days
+#
+#     def one_day(self):
+#         """
+#         Day simulation. Calls set_days to increase days count.
+#
+#         Args:
+#             random_number (int): probability 1 in 10
+#             random_index (int): random error
+#
+#         :return: set_karma
+#         :raise Exception: if random_number = 1
+#         """
+#
+#         self.set_days()
+#         print('Day {}'.format(self.__days))
+#         random_number = randint(1, 10)
+#         random_index = randint(0, 4)
+#         if random_number == 1:
+#             raise self.my_exceptions[random_index](self.my_exceptions[random_index])
+#         else:
+#             return self.set_karma(randint(1, 7))
+#
+#
+# def write_exception_to_log(string):
+#     """
+#     Write data to file.
+#
+#     :param string: name of error
+#     :type string: str
+#     """
+#
+#     with open('karma.log', 'a', encoding='utf-8') as file:
+#         file.write(string + '\n')
+#     print('{} was successfully written into karma.log\n'.format(string))
+#
+#
+# life_simulate = Life()
+# while life_simulate.get_karma() < 500:
+#     try:
+#         life_simulate.one_day()
+#     except Exception as e:
+#         print(e)
+#         write_exception_to_log(e.__str__())
+# else:
+#     print('Your carma got 500 points')
