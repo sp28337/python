79bf141fe18108bef3d6daadeb1a9ec45a7a3f2a
Pavel Tarakanov <pavel191094@icloud.com>
2024-06-27 09:37:59 +0300

    task 1 endless iterator

---
+++ 23_iterators_generators/Lesson_2.py
@@ -0,0 +1,16 @@
+# Task 1. Endless iterator
+#
+# class CountIterator:
+#     __counter = 0
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         self.__counter += 1
+#         return self.__counter
+#
+#
+# my_iter = CountIterator()
+# for i in my_iter:
+#     print(i)
