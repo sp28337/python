0f4c9a23e947e7df1aff291c287e4633a5739d80
Pavel Tarakanov <pavel191094@icloud.com>
2024-06-27 19:46:20 +0300

    task 1 endless generator

---
+++ 23_iterators_generators/Lesson_3.py
@@ -0,0 +1,12 @@
+# Task 1. Endless generator
+#
+# def endless_gen():
+#     counter = 0
+#     while True:
+#         yield counter
+#         counter += 1
+#
+#
+# gen = endless_gen()
+# for i in gen:
+#     print(i)
