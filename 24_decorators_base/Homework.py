022ed6edbfeb793f3f227f5d97645d895d051bf4
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-02 15:06:56 +0300

    task 1 How are you?

---
+++ 24_decorators_base/Homework.py
@@ -0,0 +1,14 @@
+# Task 1. How are you?
+#
+# def how_are_you(func):
+#     print('How are you? Good.\nBut I’m not very good! Okay, keep your function.')
+#     res = func
+#     return res
+#
+#
+# @how_are_you
+# def test():
+#     print('<Something is going on here...>')
+#
+#
+# test()
