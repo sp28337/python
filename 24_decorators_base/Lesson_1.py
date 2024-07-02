91f7f1436f6aa045126f29f7d8f18794edc83d20
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-02 11:31:05 +0300

    task 1 Functions

---
+++ 24_decorators_base/Lesson_1.py
@@ -0,0 +1,14 @@
+# Task 1. Functions
+# from typing import Callable, Any
+#
+#
+# def func_1(x: int) -> int:
+#     return x + 10
+#
+#
+# def func_2(func: Callable, *args, **kwargs):
+#     result = func(*args, **kwargs)
+#     return result ** 2
+#
+#
+# print(func_2(func_1, 9))
