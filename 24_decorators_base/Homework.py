4abc4f071d9ba5f552c207436ef1a1fbd19bc263
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-04 12:38:57 +0300

    task 3 Logging

--- 24_decorators_base/Homework.py
+++ 24_decorators_base/Homework.py
@@ -33,3 +33,35 @@
 #
 # print(check_web_page())
 # print(check_web_page())
+
+
+# Task 3. Logging
+# from typing import Callable, Any, List
+# import functools
+# import datetime
+#
+#
+# def logging(func: Callable) -> Callable:
+#     @functools.wraps(func)
+#     def wrapped_func(*args, **kwargs):
+#         print('Documentation of function {0}:\n{1}'.format(func.__name__, func.__doc__))
+#         try:
+#             result = func(*args, **kwargs)
+#             return result
+#         except Exception as e:
+#             print(e)
+#             date = datetime.datetime.now()
+#             with open('function_errors.log', 'a') as file:
+#                 file.write('{2}|Error <{0}> in function <{1}>\n'.format(e, func.__name__, date))
+#     return wrapped_func
+#
+#
+# @logging
+# def some_func(val: int) -> Any:
+#     """Some func"""
+#
+#     lst = val ** 3
+#     return lst
+#
+#
+# print(some_func(val=67))
