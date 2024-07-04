d6965a524049fb628893009a18706d8099d700ec
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-04 16:54:07 +0300

    task 4 Debug

--- 24_decorators_base/Homework.py
+++ 24_decorators_base/Homework.py
@@ -65,3 +65,48 @@
 #
 #
 # print(some_func(val=67))
+
+
+# Task 4. Debug
+# import functools
+#
+#
+# def debug(func):
+#     @functools.wraps(func)
+#     def wrapped_func(*args, **kwargs):
+#         print('{name}({args}{kwargs})'.format(name=func.__name__,
+#                                               args=''.join(['"{name}"'.format(name=x)
+#                                                             if len(kwargs) == 0
+#                                                             else '"{name}", '.format(name=x)
+#                                                             for x in args]
+#                                                            ),
+#                                               kwargs=', '.join(['{k}="{v}"'.format(k=key, v=val)
+#                                                                 if isinstance(val, str)
+#                                                                 else '{k}={v}'.format(k=key, v=val)
+#                                                                 for key, val in kwargs.items()]
+#                                                                )
+#                                               )
+#               )
+#         result = func(*args, **kwargs)
+#         print('"{f}" returned value "{res}"'.format(f=func.__name__, res=result))
+#         return result
+#
+#     return wrapped_func
+#
+#
+# @debug
+# def greeting(name, age=None):
+#     if age:
+#         return "Wow, {name}! You are already {age} years old, you grow fast!".format(name=name, age=age)
+#     else:
+#         return "Hello, {name}!".format(name=name)
+#
+#
+# greeting("Tom")
+# greeting("Michael", age=100)
+# greeting(name="Katty", age=16)
+# #
+# # print(greeting('Benjamin'))
+# # print(greeting('Richard', age=100))
+# print(greeting('Tom'))
+# print(greeting(name='Dorrisile', age=16))
