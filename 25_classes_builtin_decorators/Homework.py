f333436a0f457db84c4b66d8bc87215dd3cd30c4
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-09 21:21:58 +0300

    task 2 Math module

--- 25_classes_builtin_decorators/Homework.py
+++ 25_classes_builtin_decorators/Homework.py
@@ -1,4 +1,4 @@
-# Task 1. Work with file
+# Task 1. Work with file 2
 # import os
 # import _io
 #
@@ -49,3 +49,79 @@
 #     print(file.name)
 #     file.write('I gonna visit Thai very soon!')
 #     print(file.mode)
+
+
+# Task 2. Math module
+# from typing import Union
+#
+#
+# class MyMath:
+#     """
+#     Mathematics class
+#
+#     Contain functions for calculating:
+#     - sphere surface area
+#     - volume of cube
+#     - circle area
+#     - circle length
+#     """
+#     _PI = 3.14159265359
+#
+#     @property
+#     def pi(self) -> float:
+#         """ Getter pi constant """
+#         return self._PI
+#
+#     @classmethod
+#     def circle_len(cls, radius: int) -> Union[float, int]:
+#         """
+#         Contain formula fo calculating length of a circle
+#
+#         Args:
+#             radius (int): radius of a circle
+#         """
+#         result = 2 * cls._PI * radius
+#         return result
+#
+#     @classmethod
+#     def circle_sq(cls, radius: int) -> Union[float, int]:
+#         """
+#         Contain formula fo calculating area of a circle
+#
+#         Args:
+#             radius (int): radius of a circle
+#         """
+#         result = cls._PI * radius ** 2
+#         return result
+#
+#     @classmethod
+#     def sphere_sq(cls, radius: int) -> Union[int, float]:
+#         """
+#         Contain formula fo calculating area of a sphere
+#
+#         Args:
+#             radius (int): radius of a sphere
+#         """
+#         result = 4 * cls._PI * radius ** 2
+#         return result
+#
+#     @classmethod
+#     def cub_vol(cls, side_len: int) -> Union[int, float]:
+#         """
+#         Contain formula fo calculating volume of a cube
+#
+#         Args:
+#             side_len (int): length of a cube
+#         """
+#         result = side_len ** 3
+#         return result
+#
+#
+# res_1 = MyMath.circle_len(5)
+# res_2 = MyMath.circle_sq(6)
+# res_3 = MyMath.cub_vol(4)
+# res_4 = MyMath.sphere_sq(6)
+# print(res_1)
+# print(res_2)
+# print(res_3)
+# print(res_4)
