4075f3c3f9eade8ed5942029d24eb67bfe3fac61
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-20 09:48:00 +0300

    task 2 Slowing down code

--- 26_decorators_pro/Lesson_2.py
+++ 26_decorators_pro/Lesson_2.py
@@ -25,3 +25,32 @@
 #
 # some_func(10)
 
+
+# Task 2. Slowing down code
+# import math
+# import time
+# from collections.abc import Callable
+# from typing import Optional
+#
+#
+# def dx_decorator(_func: Optional[Callable] = None, *, dx: float = 0.01, sleep: int = 1) -> Callable:
+#     def func_decorator(func: Callable) -> Callable:
+#         def wrapper(x: int, *args, **kwargs) -> float:
+#             print('processing...')
+#             time.sleep(sleep)
+#             res = (func(x + dx, *args, **kwargs) - func(x, *args, **kwargs)) / dx
+#             return res
+#         return wrapper
+#     if _func is None:
+#         return func_decorator
+#     return func_decorator(_func)
+#
+#
+# @dx_decorator(sleep=0.3)
+# def sin_dx(x: int) -> float:
+#     return math.sin(x)
+#
+#
+# for d in range(1, 11):
+#     r = sin_dx(d)
+#     print(f'производная в точке {d} = {r}')
