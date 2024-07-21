94bc950c53b8e45e283364ce50a43eaa4c70cdf5
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-21 16:34:34 +0300

    task 3 ZIP again

--- 27_functional_programming/Homework.py
+++ 27_functional_programming/Homework.py
@@ -37,3 +37,12 @@
 # # print(str3)
 # # print(str1)
 # # print(str2)
+
+
+# Task 3. ZIP again
+# from typing import List
+#
+# strings: List[str] = ['a', 'b', 'c', 'd', 'e']
+# numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+# result = list(map(lambda x, y: (x, y), strings, numbers))
+# print(result)
