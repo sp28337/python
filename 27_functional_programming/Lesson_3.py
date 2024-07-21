131814126a7132e0693c2cf1469a33b79a02beea
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-21 14:12:39 +0300

    task 2 One-string code 2

--- 27_functional_programming/Lesson_3.py
+++ 27_functional_programming/Lesson_3.py
@@ -2,3 +2,9 @@
 #
 # numbers = input('Enter numbers: ').strip().split()
 # print(sorted(map(lambda x: int(x), numbers)))
+
+
+# Task 2. One-string code 2
+#
+# string = 'qWe456rtY'
+# print(list(filter(lambda x: x if x.islower() else None, string)))
