6b343d72662759de0ab74e623b6aae3df6758b59
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-21 16:06:18 +0300

    task 1 New lists

---
+++ 27_functional_programming/Homework.py
@@ -0,0 +1,17 @@
+# Task 1. New lists
+# from functools import reduce
+#
+#
+# floats = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+#
+# names = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+#
+# numbers = [22, 33, 10, 6894, 11, 2, 1]
+#
+# floats2 = list(map(lambda x: round(x**3, 3), floats))
+# names2 = list(filter(lambda x: x if len(x) >= 5 else None, names))
+# numbers2 = reduce(lambda x, y: x * y, numbers)
+#
+# print(floats2)
+# print(names2)
+# print(numbers2)
