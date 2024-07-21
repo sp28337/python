8971d8c51cc107201de1f37b1021e0e4fc049812
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-21 14:52:00 +0300

    task 3 reduce function

--- 27_functional_programming/Lesson_3.py
+++ 27_functional_programming/Lesson_3.py
@@ -8,3 +8,22 @@
 #
 # string = 'qWe456rtY'
 # print(list(filter(lambda x: x if x.islower() else None, string)))
+
+
+# Task 3. reduce function
+# from functools import reduce
+#
+#
+# def my_add(a, b):
+#     result = a + b.count('was')
+#     print(f"{a} + {b.count('was')} = {result}")
+#     return result
+#
+#
+# sentences = ["Nory was a Catholic", "because her was mother was a Catholic",
+#              "and Nory’s mother was a Catholic",
+#              "because her father was a Catholic", "and her father was a Catholic",
+#              "because his mother was a Catholic",
+#              "or had been"]
+#
+# print((reduce(my_add, sentences, 0)))
