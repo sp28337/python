87a014b40b569cf97bb18638f3a9a14f72568745
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-22 18:14:35 +0300

    task 1 Patter

---
+++ 28_libraries/Lesson_1.py
@@ -0,0 +1,29 @@
+# Task 1. Patter
+# import re
+#
+# text = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?'
+# starts_wo = re.match(r'wo', text)
+#
+# if starts_wo:
+#     print('[+] "wo"', starts_wo)
+# else:
+#     print('[-] "wo"', starts_wo)
+#
+# first_wo = re.search(r'wo', text)
+#
+# if first_wo:
+#     print('[+] "wo"', first_wo)
+# else:
+#     print('[-] "wo"', first_wo)
+#
+# print(first_wo.group(0))
+# print(first_wo.start())
+# print(first_wo.end())
+# all_wo = re.findall(r'wo', text)
+# if all_wo:
+#     print('[+]', all_wo)
+# else:
+#     print('[-]', all_wo)
+#
+# print(text)
+# print(re.sub(r'wo', r'CHANGE', text))
