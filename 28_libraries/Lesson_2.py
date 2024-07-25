e232ecd2f11953819c9ebaee53919d1261bdd4fa
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-25 07:44:13 +0300

    task 1 Consonants

---
+++ 28_libraries/Lesson_2.py
@@ -0,0 +1,7 @@
+import re
+# Task 1. Consonants
+# text = "Even if they are djinns, I will get djinns that can outdjinn them."
+# first_list = re.findall(r"\b[AEIOUaeiou]\w+\b|I", text)
+# second_list = re.findall(r"\b[^AEIOUaeio\s][a-zA-Z]*\b", text)
+# print(first_list)
+# print(second_list)
