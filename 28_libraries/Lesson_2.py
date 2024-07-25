141c540f0869d94e75110b9bc40ebb83f4292ade
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-25 08:35:12 +0300

    task 2 Date

--- 28_libraries/Lesson_2.py
+++ 28_libraries/Lesson_2.py
@@ -1,7 +1,14 @@
-import re
+# import re
 # Task 1. Consonants
 # text = "Even if they are djinns, I will get djinns that can outdjinn them."
 # first_list = re.findall(r"\b[AEIOUaeiou]\w+\b|I", text)
 # second_list = re.findall(r"\b[^AEIOUaeio\s][a-zA-Z]*\b", text)
 # print(first_list)
 # print(second_list)
+
+
+# Task 2. Date
+#
+# text = "Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009"
+# date_list = re.findall(r"\b\d\d-\d\d-\d{4}\b", text)
+# print(date_list)
