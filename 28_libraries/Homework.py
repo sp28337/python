efacce818fdf74efe8b5e994de03beadac977808
Pavel Tarakanov <pavel191094@icloud.com>
2024-07-25 11:42:13 +0300

    task 2 Registration plates

--- 28_libraries/Homework.py
+++ 28_libraries/Homework.py
@@ -10,3 +10,19 @@
 #
 # fou_letters_word = re.findall(r"\b\w{4}\b", text)
 # print(fou_letters_word)
+
+
+# Task 2. Registration plates
+# import re
+#
+#
+# reg_plates = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+#
+# pattern1 = r"\b[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d+\b"
+# pattern2 = r"\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}\b"
+#
+# private_vehicles = re.findall(pattern1, reg_plates)
+# taxi = re.findall(pattern2, reg_plates)
+#
+# print(*private_vehicles)
+# print(*taxi)
